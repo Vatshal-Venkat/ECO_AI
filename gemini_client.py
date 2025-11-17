fce793f21fc1677de062c7b6e10e96914490c12c
Venkat Vatshal <venkatvatshal@gmail.com>
2025-11-17 19:42:13 +0530

    Enhanced main.py with correct teamcenter calls and GEMINI model

--- gemini_client.py
+++ gemini_client.py
@@ -4,6 +4,6 @@ import os
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def ask_gemini(prompt: str):
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content(prompt)
     return response.text
